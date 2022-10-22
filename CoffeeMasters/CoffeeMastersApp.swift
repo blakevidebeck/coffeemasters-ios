2c404ba0c848988faf77ffabe1c6021817906824
Blake Videbeck <blake.videbeck@ezyvet.com>
2022-10-23 09:36:42 +1300

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Blake Videbeck on 23/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
