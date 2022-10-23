77548970fdd444782535d9b4630623ff67f22889
Blake Videbeck <blake.videbeck@ezyvet.com>
2022-10-24 08:55:23 +1300

    First commit

--- CoffeeMasters/ContentView.swift
+++ CoffeeMasters/ContentView.swift
@@ -1,26 +1,34 @@
-//
-//  ContentView.swift
-//  CoffeeMasters
-//
-//  Created by Blake Videbeck on 23/10/22.
-//
+	//
+	//  ContentView.swift
+	//  CoffeeMasters
+	//
+	//  Created by Blake Videbeck on 23/10/22.
+	//
 
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	@State var name = "unnamed"
+	
+	var body: some View {
+		Offer(title: "My offer", description: "This is a description")
+	}
 }
 
+//struct Greeting: View {
+//	@State var name = "unnamed"
+//
+//	var body: some View {
+//		VStack {
+//			TextField("Enter your name", text: $name)
+//			Text("Hello \(name)")
+//		}
+//
+//	}
+//}
+
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
